5a5240c49dcb77c247e4ad7bae00616b1ea226f4
Hiep Doan <strongwind352@email.com>
2025-07-17 10:45:29 +0700

    add getAllProducts admin endpoint

--- src/main/java/com/electronics/store/controller/AdminProductController.java
+++ src/main/java/com/electronics/store/controller/AdminProductController.java
@@ -1,11 +1,19 @@
 package com.electronics.store.controller;
 
+import com.electronics.store.model.Product;
+import com.electronics.store.service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class AdminProductController {
-    public void getAllProducts(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
-    }
+    ProductService productService;
 
+    public ResponseEntity<Page<Product>> getAllProducts(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> products = productService.getAllProducts(pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
--- src/main/java/com/electronics/store/service/ProductService.java
+++ src/main/java/com/electronics/store/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.electronics.store.service;
 
+import com.electronics.store.model.Product;
+import com.electronics.store.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public class ProductService {
+    ProductRepository productRepository;
+
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
 }
