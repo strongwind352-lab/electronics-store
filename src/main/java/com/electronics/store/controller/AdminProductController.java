0d115fc5ce19316f77202a49b8856e79daf2e8d6
Hiep Doan <strongwind352@email.com>
2025-07-17 10:48:41 +0700

    annotate @RestController for AdminProductController

--- src/main/java/com/electronics/store/controller/AdminProductController.java
+++ src/main/java/com/electronics/store/controller/AdminProductController.java
@@ -2,15 +2,23 @@ package com.electronics.store.controller;
 
 import com.electronics.store.model.Product;
 import com.electronics.store.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/admin/products")
+@RequiredArgsConstructor
 public class AdminProductController {
     ProductService productService;
 
+    @GetMapping
     public ResponseEntity<Page<Product>> getAllProducts(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Product> products = productService.getAllProducts(pageable);
