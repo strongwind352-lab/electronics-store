3b5becca0cd4a0f1dfbb77984cf9eeba7de8d4e3
Hiep Doan <strongwind352@email.com>
2025-07-18 23:31:55 +0700

    implement createDeal_shouldReturnCreatedDeal

--- src/main/java/com/electronics/store/dto/DealResponse.java
+++ src/main/java/com/electronics/store/dto/DealResponse.java
@@ -1,11 +1,15 @@
 package com.electronics.store.dto;
 
 import com.electronics.store.model.DealType;
-
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Value;
 
+@Value
+@Builder
 public class DealResponse {
-  private Long productId;
-  private DealType dealType;
-  private LocalDateTime expirationDate;
+  Long id;
+  Long productId;
+  DealType dealType;
+  LocalDateTime expirationDate;
 }
--- src/test/java/com/electronics/store/controller/AdminDealControllerIntegrationTest.java
+++ src/test/java/com/electronics/store/controller/AdminDealControllerIntegrationTest.java
@@ -1,10 +1,72 @@
 package com.electronics.store.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.electronics.store.model.Deal;
+import com.electronics.store.model.DealType;
+import com.electronics.store.model.Product;
+import com.electronics.store.model.ProductCategory;
+import com.electronics.store.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class AdminDealControllerIntegrationTest {}
+public class AdminDealControllerIntegrationTest {
+  @Autowired MockMvc mockMvc;
+
+  @Autowired ObjectMapper objectMapper;
+
+  @Autowired ProductRepository productRepository;
+
+  Product laptop;
+
+  @BeforeEach
+  void setUp() {
+    productRepository.deleteAll();
+    Product product =
+        new Product(
+            null, "Laptop Pro", ProductCategory.ELECTRONICS, BigDecimal.valueOf(1200.000), 10);
+    laptop = productRepository.save(product);
+  }
+
+  @Test
+  @DisplayName("createDeal - should return created deal")
+  @WithMockUser(roles = "ADMIN")
+  void createDeal_shouldReturnCreatedDeal() throws Exception {
+    // Arrange data
+    Deal newDeal = new Deal(null, laptop.getId(), DealType.BOGO50, LocalDateTime.now().plusDays(7));
+    MvcResult mvcResult =
+        mockMvc
+            .perform(
+                post("/admin/deals")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(newDeal)))
+            .andReturn();
+    mvcResult.getResponse().getContentAsString();
+    mockMvc
+        .perform(
+            post("/admin/deals")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newDeal)))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").isNumber())
+        .andExpect(jsonPath("$.productId").value(laptop.getId()))
+        .andExpect(jsonPath("$.dealType").value(DealType.BOGO50.name()));
+  }
+}
