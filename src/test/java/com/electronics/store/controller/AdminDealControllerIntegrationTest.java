f62dd26a71f7884b0ec82b79da8d459d4d4cd3e2
Hiep Doan <strongwind352@email.com>
2025-07-18 22:49:13 +0700

    add AdminDealControllerIntegrationTest.java

---
+++ src/test/java/com/electronics/store/controller/AdminDealControllerIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.electronics.store.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class AdminDealControllerIntegrationTest {}
