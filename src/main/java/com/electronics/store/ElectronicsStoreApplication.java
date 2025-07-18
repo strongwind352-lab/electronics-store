1611c5436da935f7ccde9a38a3761f3197e29bb8
Hiep Doan <strongwind352@email.com>
2025-07-18 07:20:25 +0700

    implement spring security for admin user

--- src/main/java/com/electronics/store/ElectronicsStoreApplication.java
+++ src/main/java/com/electronics/store/ElectronicsStoreApplication.java
@@ -3,9 +3,11 @@ package com.electronics.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableMethodSecurity
 public class ElectronicsStoreApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/electronics/store/controller/AdminProductController.java
+++ src/main/java/com/electronics/store/controller/AdminProductController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/products")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminProductController {
   private final ProductService productService;
 
