fa8269622277fc943a3c87591506219f51a70742
Hiep Doan <strongwind352@email.com>
2025-07-18 07:30:17 +0700

    generate AdminProductControllerIntegrationTest.java

---
+++ src/test/java/com/electronics/store/controller/AdminProductControllerIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.electronics.store.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AdminProductControllerIntegrationTest {
+
+  @BeforeEach
+  void setUp() {}
+
+  @Test
+  void getAllProducts() {}
+
+  @Test
+  void createProduct() {}
+
+  @Test
+  void deleteProduct() {}
+}
