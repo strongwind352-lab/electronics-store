6e22d3b80bbff9baeccfd7775bbfe2414dae5973
Hiep Doan <strongwind352@email.com>
2025-07-19 15:10:23 +0700

    create CustomerBasketControllerIntegrationTest.java file

---
+++ src/test/java/com/electronics/store/controller/CustomerBasketControllerIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.electronics.store.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CustomerBasketControllerIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+      }
+
+    @Test
+    void addProductToBasket() {
+      }
+}
