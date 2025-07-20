8ed572d36bc66b0b3ac0c9ff89a196041180435d
Hiep Doan <strongwind352@email.com>
2025-07-20 08:41:03 +0700

    implement createProduct_shouldCreateProductSuccessfully

--- src/test/java/com/electronics/store/service/ProductServiceTest.java
+++ src/test/java/com/electronics/store/service/ProductServiceTest.java
@@ -1,14 +1,38 @@
 package com.electronics.store.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.electronics.store.model.Product;
+import com.electronics.store.model.ProductCategory;
+import com.electronics.store.repository.ProductRepository;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    private Product laptop;
 
     @BeforeEach
     void setUp() {
+        laptop = new Product(1L, "Laptop Pro", ProductCategory.ELECTRONICS, BigDecimal.valueOf(1200.00), 10);
       }
 
     @Test
@@ -16,7 +40,18 @@ class ProductServiceTest {
       }
 
     @Test
-    void createProduct() {
+    @DisplayName("Should create product successfully")
+    void createProduct_shouldCreateProductSuccessfully() {
+        // Arrange
+        when(productRepository.save(ArgumentMatchers.any(Product.class))).thenReturn(laptop);
+
+        // Act
+        Product createdProduct = productService.createProduct(laptop);
+
+        // Assert : Verify that product was successfully created
+        assertNotNull(createdProduct);
+        assertEquals("Laptop Pro", laptop.getName());
+        verify(productRepository,times(1)).save(laptop);
       }
 
     @Test
