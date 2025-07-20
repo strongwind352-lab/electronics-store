6bb9c13a31a7d0be29a1e5350948f283f1b659f0
Hiep Doan <strongwind352@email.com>
2025-07-20 14:42:13 +0700

    create BasketServiceTest file

---
+++ src/test/java/com/electronics/store/service/BasketServiceTest.java
@@ -0,0 +1,24 @@
+package com.electronics.store.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BasketServiceTest {
+
+    @BeforeEach
+    void setUp() {
+      }
+
+    @Test
+    void addProductToBasket() {
+      }
+
+    @Test
+    void removeProductFromBasket() {
+      }
+
+    @Test
+    void calculateReceipt() {
+      }
+}
