456cf802da9c916beb56abc9fb667f91c8df61cc
Hiep Doan <strongwind352@email.com>
2025-07-20 08:27:26 +0700

    create ProductServiceTest file

---
+++ src/test/java/com/electronics/store/service/ProductServiceTest.java
@@ -0,0 +1,41 @@
+package com.electronics.store.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductServiceTest {
+
+    @BeforeEach
+    void setUp() {
+      }
+
+    @Test
+    void getAllProducts() {
+      }
+
+    @Test
+    void createProduct() {
+      }
+
+    @Test
+    void removeProduct() {
+      }
+
+    @Test
+    void filterProducts() {
+      }
+
+    @Test
+    void findProductById() {
+      }
+
+    @Test
+    void decrementProductStock() {
+      }
+
+    @Test
+    void incrementProductStock() {
+      }
+}
